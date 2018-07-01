768eebbea73d23342861c973409d88b44bcf0ff8
HEZBON <hezbon.oduol@aiesec.net>
2018-07-01 16:52:01 +0300

    modifying

--- designs.js
+++ designs.js
@@ -1,29 +1,28 @@
 // Select color input
 // Select size input
-var height,width,color;
+var a,b,color;//height=a,width=b
 // When size is submitted by the user, call makeGrid()
-$('#sizePicker').submit(function (event){
-   event.preventDefault();
+$('#sizePicker').submit(function (e){
+   e.preventDefault();
    a = $('#inputHeight').val();
    b = $('#inputWeight').val();
    makeGrid(a,b);
-})
-function makeGrid(a,b) {
-  $('tr').remove();
-//console.log('Height:'+ height + 'and width:' + width);
-// Your code goes here!
-for (var i = 1; i <= a; i++) {
-     $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
-     for (var j = 1; j <= b; j++) {
-       $('#table' + i).append('<td></td>');
-}
-}
+ })
+ // Your code goes here!
+ function makeGrid(a,b) {
+   $('tr').remove();//remove saved table when inputing new table values
+ for (var tr = 1; tr <= a; tr++) {//build table rows
+      $('#pixelCanvas').append('<tr class=rows' + tr + '></tr>');
+      for (var td = 1; td <= b; td++) {//input cells into table rows
+        $('.rows' + tr).append('<td></td>');
+ }
+ }
 $('td').click(function addColor(){
   color=$('#colorPicker').val();
   if($(this).attr('style')){
     $(this).removeAttr('style');
   }else{
-    $(this).attr('style','background-color:' + color);
+    $(this).attr('style','background-color',color);
   }
 })
 }
